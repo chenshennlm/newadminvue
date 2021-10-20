e0c4f8399fda6c4a4c042b357f938ac1a395d7a2
cs15738190781nlm <862303687@qq.com>
2021-10-20 09:42:20 +0800

    构建项目、配置全局样式 路由 element-ui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="login">
+      这里是登录页
+       <el-button disabled>默认按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+    #login{
+      height: 100vh;
+     background: #344a5f;
+    }
+</style>
